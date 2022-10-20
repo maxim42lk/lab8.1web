915b455f9598f3e60602c49645976a7336cadb73
maxim42lk <74965865+maxim42lk@users.noreply.github.com>
2022-10-20 20:37:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Введите его/ее имя');
+let secondName = prompt('Введите его/ее имя');
+var result = Math.random() * 100;
+result = Math.floor(result);
+alert(`${firstName} подходит ${secondName} на ${result} процентов!`);
